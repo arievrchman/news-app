15f9f371a9efcf908959822f8081ed02b203f287
rachmadza <razato.zai@gmail.com>
2019-02-08 15:38:10 +0700

    fixed auth user

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,15 +2,19 @@ const express = require('express')
 const router = express.Router()
 const YandexController = require('../controllers/yandex')
 const NewsapiController = require('../controllers/newsapi')
+const auth = require('../middlewares/authUser')
 
+
+
+router.get('/', (req, res) => {
+    res.status(404).json({ msg: 'page not found' })
+})
+
+router.use(auth)
 router.get('/api/news/top-headlines', NewsapiController.getTopHeadlines)
 router.get('/api/news/everything', NewsapiController.getEverything)
 router.get('/api/news/sources', NewsapiController.getSources)
 
 router.get('/api/translate/:lang/:text', YandexController.translate)
-
-router.get('/', (req, res) => {
-    res.status(404).json({ msg: 'page not found' })
-})
 
 module.exports = router
--- server/routes/route_user.js
+++ server/routes/route_user.js
@@ -2,5 +2,6 @@ const router = require('express').Router()
 const UserController = require('../controllers/controller_user')
 
 router.post('/auth/google', UserController.signIn)
+router.post('/user/verify', UserController.verify)
 
 module.exports = router
